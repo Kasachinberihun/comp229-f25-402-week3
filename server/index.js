ceec3a96788b0aa02445e3d3879d6fec7ec0132e
Kasachinberihun <kasachinberihun@gmail.com>
2025-10-13 00:54:50 -0400

    Set up Express server in /server with homeRoutes; Vite client working

---
+++ server/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import morgan from 'morgan';
+
+import homeRoutes from './routes/homeRoutes.js';
+
+
+const app = express();
+
+app.use(morgan('dev'));
+
+//Routes
+app.use('/',homeRoutes);
+
+
+
+app.listen(3000);
+
+console.log('Server running on http://localhost:3000/');
+
+
